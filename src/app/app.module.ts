0222bac590c8a37e8a3449747a4f4a640628c8f0
Blai <blai.samitier@gmail.com>
2018-01-30 13:11:47 +0100

    dimarts 30/1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from './app.component'
+import { RouterModule } from '@angular/router'
+import { routes } from './routes'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		RouterModule.forRoot(routes),
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
